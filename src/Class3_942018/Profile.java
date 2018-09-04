fb749d2d4678b423390d385b7857f8befb4f1c6b
yapalfredo <36459847+yapalfredo@users.noreply.github.com>
2018-09-04 18:17:28 -0400

    commit for additional constructor

--- src/Class3_942018/Profile.java
+++ src/Class3_942018/Profile.java
@@ -14,25 +14,38 @@ public class Profile {
     private String fName;
     private String lName;
     private String userName;
-    private String major;
     private String classRank;
     private int numberOfCourses;
     private String semester;
+    private String major;
+    private byte creditsThisSemester;
 
 //Constructors
     public Profile(){
         
     }
 
-    public Profile(String fName, String lName, String userName, String major, String classRank, int numberOfCourses, String semester) {
+    public Profile(String fName, String lName, String userName, String classRank, int numberOfCourses, String semester, String major, byte creditsThisSemester) {
         this.fName = fName;
         this.lName = lName;
         this.userName = userName;
-        this.major = major;
         this.classRank = classRank;
         this.numberOfCourses = numberOfCourses;
         this.semester = semester;
+        this.major = major;
+        this.creditsThisSemester = creditsThisSemester;
     }
+
+    public Profile(String fName, String lName, String userName, String semester, String major, byte creditsThisSemester) {
+        this.fName = fName;
+        this.lName = lName;
+        this.userName = userName;
+        this.semester = semester;
+        this.major = major;
+        this.creditsThisSemester = creditsThisSemester;
+    }
+
+ 
 // Getters
     public String getfName() {
         return fName;
@@ -61,7 +74,11 @@ public class Profile {
     public String getSemester() {
         return semester;
     }
-
+    
+    public byte getCreditsThisSemester() {
+        return creditsThisSemester;
+    }
+    
 //Setters
     public void setfName(String fName) {
         this.fName = fName;
@@ -91,5 +108,8 @@ public class Profile {
         this.semester = semester;
     }
     
-    
+    public void setCreditsThisSemester(byte creditsThisSemester) {
+        this.creditsThisSemester = creditsThisSemester;
+    }
+        
 }
