a94d58abc7c357fd4cd19e9fd0e886901b504ee2
yapalfredo <36459847+yapalfredo@users.noreply.github.com>
2018-09-04 19:00:53 -0400

    7pm - added object in main class

--- src/Class3_942018/Main.java
+++ src/Class3_942018/Main.java
@@ -5,6 +5,8 @@
  */
 package Class3_942018;
 
+import java.util.HashSet;
+
 /**
  *
  * @author al
@@ -16,6 +18,27 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Profile student1 = new Profile("Alfredo", "Yap",  "AYap", "Sophomore", "CSC", 13);
+        
+        
+        Profile student2 = new Profile("Gabriel", "Chimbo", "gchimbo", "Fall2018", "CSC", 12);
+        student2.setClassRank("Sophomore");
+        student2.setCreditsThisSemester(5);
+        
+        Profile student3 = new Profile();
+        student3.setfName("Brianna");
+        student3.setlName("Neura");
+        student3.setUserName("BNeura");
+        student3.setSemester("Fall2018");
+        student3.setMajor("MAT");
+        student3.setClassRank("Sophomore");
+        student3.setCreditsThisSemester(12);
+        student3.setNumberOfCourses(4);
+        
+        
+        
+        
     }
     
 }
