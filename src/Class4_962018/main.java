181b85b9178f4d3aafd4114f957038aa06f9368e
yapalfredo <36459847+yapalfredo@users.noreply.github.com>
2018-09-13 17:25:19 -0400

    9/13/2018

--- src/Class4_962018/main.java
+++ src/Class4_962018/main.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.BufferedReader;
 
 /**
  *
@@ -21,18 +22,37 @@ public class main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        try{
-        File file = new File("C:\\Users\\al\\OneDrive - Borough of Manhattan Community College\\CS 210\\MSFT.csv");
-        FileReader filereader = new FileReader(file);
-        
-    }catch (FileNotFoundException e){
-        e.printStackTrace();
-    }catch(IOException io){
-        io.printStackTrace();
-    }finally {
+
+        try {
+            File file = new File("C:\\Users\\al\\OneDrive - Borough of Manhattan Community College\\CS 210\\MSFT.csv");
+            FileReader filereader = new FileReader(file);
+            BufferedReader bf = new BufferedReader(filereader);
+            
+            String line = "";
+            
+            while((line = bf.readLine()) != null){
+       n  
+                int locationOfFirstComma = line.indexOf(",");
+                int locationOfSecondComma = line.indexOf(",",locationOfFirstComma+1);
+                String open = line.substring(locationOfFirstComma+1, locationOfSecondComma);
+                double openValue;
+                try{
+                     openValue = Double.parseDouble(open);
+                }catch (NumberFormatException e){
+                    System.out.println(e.getLocalizedMessage());
+                    openValue = 0;
+                }
+                System.out.println(openValue*2);
+                
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException io) {
+            io.printStackTrace();
+        } finally {
             System.out.println("We read the file successfully");
         }
-    
- }
+
+    }
 }
