c444aca13b6b7c7dd5b773f7925edf07a5c60726
yapalfredo <36459847+yapalfredo@users.noreply.github.com>
2018-09-13 18:16:26 -0400

    9/13/2018

--- src/Class4_962018/main.java
+++ src/Class4_962018/main.java
@@ -27,31 +27,56 @@ public class main {
             File file = new File("C:\\Users\\al\\OneDrive - Borough of Manhattan Community College\\CS 210\\MSFT.csv");
             FileReader filereader = new FileReader(file);
             BufferedReader bf = new BufferedReader(filereader);
-            
+
             String line = "";
-            
-            while((line = bf.readLine()) != null){
-       n  
+            bf.readLine();
+            while ((line = bf.readLine()) != null) {
+               
+                
                 int locationOfFirstComma = line.indexOf(",");
-                int locationOfSecondComma = line.indexOf(",",locationOfFirstComma+1);
-                String open = line.substring(locationOfFirstComma+1, locationOfSecondComma);
-                double openValue;
-                try{
-                     openValue = Double.parseDouble(open);
+                int locationOfSecondComma = line.indexOf(",", locationOfFirstComma + 1);
+                int locationOfThirdComma = line.indexOf(",", locationOfSecondComma + 1);
+                int locationOfFourthComma = line.indexOf(",", locationOfThirdComma + 1);
+                int locationOfFifthComma = line.indexOf(",", locationOfFourthComma + 1);
+                int locationOfSixthComma = line.indexOf(",", locationOfFifthComma + 1);
+
+                String date = line.substring(0,locationOfFirstComma);
+                String open = line.substring(locationOfFirstComma + 1, locationOfSecondComma);
+                String high = line.substring(locationOfSecondComma + 1, locationOfThirdComma);
+                String low = line.substring(locationOfThirdComma + 1, locationOfFourthComma);
+                String close = line.substring(locationOfFourthComma + 1, locationOfFifthComma);
+                String adjustedClose = line.substring(locationOfFifthComma+1, locationOfSixthComma);
+                String volume = line.substring(locationOfSixthComma+1);
+                
+
+                double dblOpen, dblHigh, dblLow, dblClose,dblAdjClose;
+                int intVolume;
+                try{                   
+                     dblOpen = Double.parseDouble(open);
+                     dblHigh = Double.parseDouble(high);
+                     dblLow = Double.parseDouble(low);
+                     dblClose = Double.parseDouble(close);
+                     dblAdjClose = Double.parseDouble(adjustedClose);
+                     intVolume = Integer.parseInt(volume);
                 }catch (NumberFormatException e){
                     System.out.println(e.getLocalizedMessage());
-                    openValue = 0;
+                    dblOpen = 0;
+                    dblHigh = 0;
+                    dblLow = 0;
+                    dblClose = 0;
+                    dblAdjClose = 0;
+                    intVolume=0;
                 }
-                System.out.println(openValue*2);
-                
+               System.out.println(date + " " + dblOpen + " " + dblHigh + " " + dblLow + " " + dblClose + " "+ dblAdjClose + " "+ intVolume);
+               
             }
 
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+             e.printStackTrace();
         } catch (IOException io) {
-            io.printStackTrace();
+              io.printStackTrace();
         } finally {
-            System.out.println("We read the file successfully");
+              System.out.println("We read the file successfully");
         }
 
     }
