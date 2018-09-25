c5b223b5c75320c5694506c88d8262a24b8ffd6f
yapalfredo <36459847+yapalfredo@users.noreply.github.com>
2018-09-25 17:39:19 -0400

    9/17/2018

--- src/Class6_9202018/main.java
+++ src/Class6_9202018/main.java
@@ -11,4 +11,26 @@ package Class6_9202018;
  */
 public class main {
     
+    
+    public static void main(String[]args){
+  
+    int temp1, a, b;
+    
+    int[] temp2, temp3, temp4, temp;
+    
+    int anotherWay[] = { 1,2,3,4,5 };
+    
+   
+    temp = new int[5];
+    
+    temp[0] = 1;
+    temp[1] = 2;
+    temp[2] = 3;
+    temp[3] = 4;
+    temp[4] = 5;
+    
+    
+    }
+  
+    
 }
