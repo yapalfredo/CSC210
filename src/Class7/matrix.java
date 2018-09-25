ce33053c9434b95e08064d17e05acb056082df8b
yapalfredo <36459847+yapalfredo@users.noreply.github.com>
2018-09-25 19:17:29 -0400

    9/25/2018

--- src/Class7/matrix.java
+++ src/Class7/matrix.java
@@ -18,8 +18,7 @@ public class matrix {
     
     public static void tenByTen(){
         
-        int a[][] = new int[10][10];
-        
+       
         for (int row = 1; row <=10; row++) {
             for (int col = 1; col <=10; col++) {
               //  System.out.print((row * col) + " ");
